97ae955bb631757002f311e80de5c4f8f94a929e
Sagar Tandan <076bct072@ioepc.edu.np>
2024-04-29 08:51:47 +0545

    Updated

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -3,19 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-import dotenv from 'dotenv';
-
-
 
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "blog-d0133.firebaseapp.com",
-  projectId: "blog-d0133",
-  storageBucket: "blog-d0133.appspot.com",
-  messagingSenderId: process.env.MESSESING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
