c7fe5b8219af7e966df8330a50d7a16ba49b8b4d
Sagar Tandan <076bct072@ioepc.edu.np>
2024-04-29 09:53:55 +0545

    try

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -10,20 +10,20 @@ require('dotenv').config();
 
 const firebaseConfig = {
   // apiKey: import.meta.env.VITE_API_KEY,
-  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_APP_ID,
-  // measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 
-  apiKey: process.env.VITE_API_KEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
-  measurementId: process.env.VITE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: process.env.VITE_AUTH_DOMAIN,
+  // projectId: process.env.VITE_PROJECT_ID,
+  // storageBucket: process.VITE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  // appId: process.env.VITE_APP_ID,
+  // measurementId: process.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+  },
 })
