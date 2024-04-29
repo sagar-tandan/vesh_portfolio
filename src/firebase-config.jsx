2ac76db67222a729f3065b42f84c164472ec8594
Sagar Tandan <076bct072@ioepc.edu.np>
2024-04-29 13:19:04 +0545

    try

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 // dotenv.config();
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_API_KEY,
+   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+//   define: {
+//     'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+
 export default defineConfig({
+  // other vite configurations...
   plugins: [react()],
-  define: {
-    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+
+  build: {
+    // Inject environment variables into the Vite project
+    rollupOptions: {
+      // Make sure to replace process.env.FIREBASE_API_KEY with your actual environment variable name
+      plugins: [
+        require('rollup-plugin-replace')({
+          'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        }),
+      ],
+    },
   },
-})
+});
