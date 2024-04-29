a34c5abd0abf6515824ec01fc73663ba88aefaa2
Sagar Tandan <076bct072@ioepc.edu.np>
2024-04-29 09:41:40 +0545

    jpt

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -3,8 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
 const firebaseConfig = {
